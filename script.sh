b823cee97601bc01ef561a08c404ffaeec730dcd
pdrion <philippedrion@gmail.com>
2020-02-06 19:41:11 +0100

    update emacs

--- script.sh
+++ script.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-ln -s ~/dotfiles/.emacs ~/.emacs
-ln -s ~/dotfiles/config.org ~/config.org
-ln -s ~/dotfiles/list.el ~/list.el
-ln -s ~/dotfiles/string.el ~/string.el
-ln -s ~/dotfiles/comments.el ~/comments.el
-ln -s ~/dotfiles/header.el ~/header.el
+ln -sf ~/dotfiles/.emacs.d/ ~/.emacs.d
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.bashrc ~/.bashrc
